fbfa0412b5713a92cd699ca66ecc4be563d15cb9
Aviral Singh <aviralsingh99@gmail.com>
2023-09-12 15:37:28 +0530

    Time: 11 ms (50.44%), Space: 21.7 MB (69.79%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,55 +11,20 @@
  */
 class Solution {
 public:
-    bool ans = true;
-    pair<int,int>  check(TreeNode* root)
+    bool check(TreeNode* root, TreeNode* min, TreeNode* max)
     {
-        if(ans == false) return make_pair(0,0);
-        pair<int,int> a,b;
-        int min, max; 
-        if(root->left == NULL && root->right == NULL)
+        if(root == NULL) return  true;
+        if((min == NULL || min->val < root->val) && (max == NULL || max->val > root->val))
         {
-            return make_pair(root->val,root->val);
+            cout<<root->val<<"\n";
+            return check(root->left,min,root) && check(root->right,root,max);
         }
-        else if(root->left != NULL && root->right == NULL)
-        {
-            a = check(root->left);
-            min = a.first;
-            max = a.second;
-            if(root->val <= max)
-            {
-                ans = false;
-            }
-            if(min > root->val) min = root->val;
-            if(max < root->val) max = root->val;
-        }
-        else if(root->left == NULL && root->right != NULL)
-        {
-            a = check(root->right);
-            min = a.first;
-            max = a.second;
-            if(root->val >=  min)
-            {
-                ans = false;
-            }
-            if(min > root->val) min = root->val;
-            if(max < root->val) max = root->val;
-        }
-        else
-        {
-            a = check(root->left);
-            b = check(root->right);
-            // cout<<a.first<<" "<<a.second<<" "<<b.first<<" "<<b.second<<"\n";
-            min = a.first;
-            max = b.second;
-            if(root->val <= a.second || root->val >= b.first) ans = false;
-            if(min > root->val) min = root->val;
-            if(max < root->val) max = root->val;
-        }
-        return make_pair(min,max);
+        return false;
     }
     bool isValidBST(TreeNode* root) {
-        check(root);
-        return ans;
+        ios_base::sync_with_stdio(0);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        return check(root, NULL, NULL);
     }
 };
