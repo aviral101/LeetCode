4792c7de8b6e04c4e93881be17d8d4c126e1f241
Aviral Singh <aviralsingh99@gmail.com>
2023-09-14 14:23:45 +0530

    Time: 3 ms (15.4%), Space: 6.5 MB (6.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> v(n+1);
+        v[1] = 1;
+        v[0] = 1;
+        for(int i = 2; i<=n; i++)
+        {
+          v[i] = v[i-1] + v[i-2];
+        }
+        return v[n];
+    }
+};
