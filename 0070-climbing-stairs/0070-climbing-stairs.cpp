99867c752ae02c930e1f157bedf5a5bbaea007ee
Aviral Singh <aviralsingh99@gmail.com>
2023-09-14 14:25:15 +0530

    Time: 0 ms (100%), Space: 6.7 MB (6.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         vector<int> v(n+1);
         v[1] = 1;
         v[0] = 1;
