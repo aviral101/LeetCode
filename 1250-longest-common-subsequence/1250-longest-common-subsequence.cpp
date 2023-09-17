3106c4a81dbebcbbd6d678057bfc3d0a930a08af
Aviral Singh <aviralsingh99@gmail.com>
2023-09-18 00:40:47 +0530

    Time: 14 ms (98.51%), Space: 11.1 MB (74.32%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-    int d[1001][1001];
-    int dp(string s1, int i, string s2, int j)
-    {
-        if(i < 0 || j < 0)
-        {
-            return 0;
-        }
-        if(d[i][j] != -1) return d[i][j];
-        if(s1[i] == s2[j])
-        {
-            return d[i][j] = 1 + dp(s1,i-1,s2,j-1);
-        }
-        else
-        {
-            return d[i][j] = max(dp(s1,i-1,s2,j), dp(s1,i,s2,j-1));
-        }
-    }
     int longestCommonSubsequence(string text1, string text2) {
         ios_base :: sync_with_stdio(0);
         cin.tie(nullptr);
         cout.tie(nullptr);
         int n = text1.size();
         int m = text2.size();
-        for(int i = 0; i<1001; i++)
-        {
-            for(int j = 0; j<1001; j++)
-            {
-                d[i][j] = 0;
-            }
-        }
+        int d[n+1][m+1];
+        memset(d,0,sizeof(d));
         for(int i = 1; i<=n; i++)
         {
             for(int j = 1; j<=m; j++)
