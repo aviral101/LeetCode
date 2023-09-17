e869a3bd1783a1159d90004966844775fe638e02
Aviral Singh <aviralsingh99@gmail.com>
2023-09-17 10:40:33 +0530

    Time: 40 ms (54.55%), Space: 55.4 MB (18.18%) - LeetHub

--- 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.cpp
+++ 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.cpp
@@ -2,44 +2,62 @@ class Solution {
 public:
     int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
         int ma = 0;
-        int l =0, r = INT_MAX-1;
-        while(l < r)
+        for(int i = 0; i<k; i++)
         {
-            int mid = l + (r - l + 1)/2;
-            int flag = -1;
-            for(int  i = 0; i<k; i++)
+            unsigned long long co;
+            long long l = 0, r = 10000000000;
+            while(l<r)
             {
-                unsigned long long c = 0;
-                
-                for(int j = 0; j<n; j++)
+                long long mid = l + (r - l + 1)/2;
+                co = 0;
+                // cout<<l<<" "<<r<<"\n";
+                // cout<<mid<<" : mid\n";
+                for(int j = 0;j <n; j++)
                 {
                     unsigned long long a = composition[i][j];
-                    a = a * (unsigned long long)mid;
+                    a = (unsigned long long)mid * (unsigned long long)a;
                     if(a > stock[j])
                     {
                         a = a - stock[j];
-                        c += a * (unsigned long long)cost[j];
+                        co += ((unsigned long long)a)*((unsigned long long)cost[j]);
+                        if(co > budget)
+                        {
+                            break;
+                        }
                     }
-                    if(c > budget)
+                    
+                }
+                // cout<<co<<" : cost\n";
+                if(co > budget)
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid;
+                }
+            }
+            co = 0;
+            l = r;
+            for(int j = 0;j <n; j++)
+            {
+                unsigned long long a = composition[i][j];
+                a = (unsigned long long)l * a;
+                if(a > stock[j])
+                {
+                    a = a - stock[j];
+                    co += ((unsigned long long)a)*((unsigned long long)cost[j]);
+                    if(co > budget)
                     {
+                        co = 10*budget;
                         break;
                     }
                 }
-                if(c <= budget)
-                {
-                    flag = 1;
-                    break;
-                }
-            } 
-            if(flag == 1)
-            {
-                l = mid;
+
             }
-            else
-            {
-                r = mid-1;
-            }
+            // cout<<l<<" "<<r<<"\n";
+            if(co <= budget && ma < l) ma = l;
         }
-        return l;
+        return ma;
     }
 };
