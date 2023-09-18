9e0f55b97073a7284e7c80ea924de97f56693cf7
Aviral Singh <aviralsingh99@gmail.com>
2023-09-18 23:32:38 +0530

    Time: 6 ms (97.39%), Space: 7.9 MB (74.74%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string text1, string text2) {
+        ios_base :: sync_with_stdio(0);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int n = text1.size();
+        int m = text2.size();
+        int d[n+1][m+1];
+        memset(d,0,sizeof(d));
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                d[i][j] = (text1[i-1] == text2[j-1])? 1+d[i-1][j-1] : max(d[i-1][j],d[i][j-1]);
+            }
+        }
+        
+        return n - d[n][m] + m - d[n][m];
+    }
+};
