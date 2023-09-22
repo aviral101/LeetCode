fd47e7d827fb95924021537aa70f93f63e1eb534
Aviral Singh <aviralsingh99@gmail.com>
2023-09-23 00:03:12 +0530

    Time: 1324 ms (5.02%), Space: 24.7 MB (56.28%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int d[2001][2001];
+    int dp(string &s, int i, string &p, int j)
+    {
+        if(i == s.size())
+        {
+            for(int k = j; k<p.size(); k++)
+            {
+                if(p[k] != '*') return d[i][j] = 0;
+            }
+            return d[i][j] = 1;
+        }
+        if(i == s.size() && j == p.size()) return 1;
+        else if(i == s.size() || j == p.size()) return 0;
+        if(d[i][j] != -1) return d[i][j];
+        if(p[j] != '*' && p[j] != '?')
+        {
+            if(s[i] == p[j]) return d[i][j] = dp(s, i+1, p, j+1);
+            else
+            {
+                return d[i][j] = 0;
+            }
+        }
+        else if(p[j] == '?')
+        {
+            return d[i][j] = (dp(s, i+1, p, j+1));
+        }
+        else if(p[j] == '*')
+        {
+            return d[i][j] = (dp(s,i,p,j+1) | dp(s,i+1,p,j));
+        }
+        return 0;
+    }
+    bool isMatch(string s, string p) {
+        memset(d,-1,sizeof(d));
+        return dp(s, 0, p , 0);
+    }
+};
