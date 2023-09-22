bafd99e8a87dd42c035059371fc23c5e50d136a1
Aviral Singh <aviralsingh99@gmail.com>
2023-09-22 21:27:01 +0530

    Time: 29 ms (86.55%), Space: 11.4 MB (71.74%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int d[1001][1001];
+    int dp(string &s, int l, int r)
+    {
+        if(l == r) return 1;
+        if(l > r) return 0;
+        if(d[l][r] != -1) return d[l][r];
+        if(s[l] == s[r])
+        {
+              return d[l][r] = 2 + dp(s,l+1,r-1);
+        }
+        else
+        {
+            return d[l][r] = max(dp(s,l+1,r),dp(s,l,r-1));
+        }
+    }
+    int minInsertions(string s) {
+        int n = s.size();
+        memset(d,-1,1001 * 1001 * sizeof(int));
+        return n - dp(s,0,n-1);
+    }
+};
